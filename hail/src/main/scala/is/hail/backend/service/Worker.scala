0058472f667390387bbbaf98ef762173cbc02d32
Daniel Goldstein <danielgold95@gmail.com>
2021-04-05 14:14:52 -0400

    more debug info

--- hail/src/main/scala/is/hail/backend/service/Worker.scala
+++ hail/src/main/scala/is/hail/backend/service/Worker.scala
@@ -64,10 +64,13 @@ object Worker {
     timer.start("readInputs")
 
     val fs = retryTransientErrors {
+      log.info("Creating filesystem")
       using(new FileInputStream(s"$scratchDir/gsa-key/key.json")) { is =>
         new GoogleStorageFS(IOUtils.toString(is, Charset.defaultCharset().toString())).asCacheable()
       }
     }
+
+    log.info(s"FileSystem that we ended up with: ${fs.getClass()}")
 
     val f = retryTransientErrors {
       using(new ObjectInputStream(fs.openNoCompression(s"$root/f"))) { is =>
