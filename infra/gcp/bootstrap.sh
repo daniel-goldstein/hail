f68c242262f532137c1b17c65330dafdbaaa9a86
Daniel Goldstein <danielgold95@gmail.com>
2021-11-02 15:42:19 -0400

    update instructions

--- infra/gcp/bootstrap.sh
+++ infra/gcp/bootstrap.sh
@@ -2,4 +2,13 @@
 
 source ../bootstrap_utils.sh
 
+function configure_gcloud() {
+    ZONE=${1:-"us-central1-a"}
+
+    gcloud -q auth configure-docker
+    # If you are using the Artifact Registry:
+    # gcloud -q auth configure-docker $REGION-docker.pkg.dev
+    gcloud container clusters get-credentials --zone $ZONE vdc
+}
+
 "$@"
