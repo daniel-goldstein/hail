612d1ce5bec1a040434e9fbbd803477683690717
Daniel Goldstein <danielgold95@gmail.com>
2022-09-15 21:39:10 -0400

    [docker] Make sure hailgenetics images are mirrored on deploy

--- infra/bootstrap_utils.sh
+++ infra/bootstrap_utils.sh
@@ -10,12 +10,8 @@ fi
 source $HAIL/devbin/functions.sh
 
 copy_images() {
-    cd $HAIL/docker/third-party
-    DOCKER_PREFIX=$(get_global_config_field docker_prefix)
-    DOCKER_PREFIX=$DOCKER_PREFIX ./copy_images.sh
-    cd -
-
-    make -C $HAIL/docker/python-dill push DOCKER_PREFIX=$DOCKER_PREFIX
+    make -C $HAIL/docker/third-party copy
+    make -C $HAIL/docker/hailgenetics publish-python-dill
 }
 
 generate_ssl_certs() {
