8e27aa4cb5337234621762c8c6def6c89fe09ab1
Daniel Goldstein <danielgold95@gmail.com>
2022-09-16 09:21:57 -0400

    [docker] Mirror hailgenetics images in AR mirrors in addition to DOCKER_PREFIX

--- docker/hailgenetics/mirror_images.sh
+++ docker/hailgenetics/mirror_images.sh
@@ -16,6 +16,8 @@ then
     exit 1
 fi
 
+MIRROR_REPOSITORIES=$@
+
 python_dill_images=(
     "python-dill:3.7"
     "python-dill:3.7-slim"
@@ -30,6 +32,10 @@ python_dill_images=(
 for image in "${python_dill_images[@]}"
 do
     copy_image "hailgenetics/${image}" "${DOCKER_PREFIX}/hailgenetics/${image}"
+    for repository in ${MIRROR_REPOSITORIES}
+    do
+        copy_image "hailgenetics/${image}" "${repository}/hailgenetics/${image}"
+    done
 done
 
 pip_release_images=(
@@ -42,4 +48,11 @@ do
     then
         echo "Could not find DockerHub image ${image}. Images for ${HAIL_PIP_VERSION} might not be published yet."
     fi
+    for repository in ${MIRROR_REPOSITORIES}
+    do
+        if ! copy_image "hailgenetics/${image}" "${repository}/hailgenetics/${image}";
+        then
+            echo "Could not find DockerHub image ${image}. Images for ${HAIL_PIP_VERSION} might not be published yet."
+        fi
+    done
 done
--- infra/gcp/bootstrap.sh
+++ infra/gcp/bootstrap.sh
@@ -5,9 +5,11 @@ source ../bootstrap_utils.sh
 function configure_gcloud() {
     ZONE=${1:-"us-central1-a"}
 
-    gcloud -q auth configure-docker
-    # If you are using the Artifact Registry:
-    # gcloud -q auth configure-docker $REGION-docker.pkg.dev
+    REGIONS=$(get_global_config_field batch_gcp_regions)
+    for region in $(echo $REGIONS | jq -r '.[]');
+    do
+        gcloud -q auth configure-docker $region-docker.pkg.dev
+    done
     gcloud container clusters get-credentials --zone $ZONE vdc
 }
 
